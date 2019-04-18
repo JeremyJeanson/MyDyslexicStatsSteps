60895aae2e8ca7453b0045a42d426dd92fa095e7
Jeremy Jeanson <jeremy.jeanson@live.fr>
2019-04-18 11:56:10 +0200

    Target Fitbit SDK 3.1 and add Versa Light support

--- Sources/app/index.ts
+++ Sources/app/index.ts
@@ -20,8 +20,10 @@ const dates = dateContainer.getElementsByTagName("image") as ImageElement[];
 const cloks = document.getElementById("clock-container").getElementsByTagName("image") as ImageElement[];
 
 // Battery
-const batteryValue = document.getElementById("battery-bar-value") as GradientRectElement;
-const batteries = document.getElementById("battery-container").getElementsByTagName("image") as ImageElement[];
+const _batteryBarContainer = document.getElementById("battery-bar-container") as GraphicsElement;
+const _batteryBar = document.getElementById("battery-bar-value") as GradientRectElement;
+const _batteriesContainer = document.getElementById("battery-container") as GraphicsElement;
+const _batteries = _batteriesContainer.getElementsByTagName("image") as ImageElement[];
 
 // Stats
 // const arcSteps = document.getElementById("arc-steps") as ArcElement;
@@ -32,7 +34,7 @@ const stepsContainer = document.getElementById("steps-container") as GraphicsEle
 const hrmContainer = document.getElementById("hrm-container") as GroupElement;
 const iconHRM = document.getElementById("iconHRM") as GraphicsElement;
 const imgHRM = document.getElementById("icon") as ImageElement;
-const hrmTexts = document.getElementById("hrm-text-container") .getElementsByTagName("image") as ImageElement[];
+const hrmTexts = document.getElementById("hrm-text-container").getElementsByTagName("image") as ImageElement[];
 
 // --------------------------------------------------------------------------------
 // Clock
@@ -43,29 +45,29 @@ simpleMinutes.initialize("seconds", (hours, mins, date) => {
   // mins="38";
   // date = "jan 17";
   // Hours
-  if(hours) {
-    cloks[0].href = util.getImageFromLeft(hours,0);
-    cloks[1].href = util.getImageFromLeft(hours,1);
+  if (hours) {
+    cloks[0].href = util.getImageFromLeft(hours, 0);
+    cloks[1].href = util.getImageFromLeft(hours, 1);
   }
 
   // Minutes
-  if(mins) {    
-    cloks[3].href = util.getImageFromLeft(mins,0);
-    cloks[4].href = util.getImageFromLeft(mins,1);  
+  if (mins) {
+    cloks[3].href = util.getImageFromLeft(mins, 0);
+    cloks[4].href = util.getImageFromLeft(mins, 1);
   }
 
   // Date
-  if(date) {
+  if (date) {
     // Position
     dateContainer.x = (device.screen.width) - (date.length * 20);
     // Values
-    for(let i=0; i<dates.length; i++){
+    for (let i = 0; i < dates.length; i++) {
       dates[i].href = util.getImageFromLeft(date, i);
     }
   }
 
-    // update od stats
-    UpdateActivities();
+  // update od stats
+  UpdateActivities();
 });
 
 // --------------------------------------------------------------------------------
@@ -74,15 +76,15 @@ simpleMinutes.initialize("seconds", (hours, mins, date) => {
 import * as batterySimple from "./simple/power-battery";
 
 // Method to update battery level informations
-batterySimple.initialize((battery)=>{
+batterySimple.initialize((battery) => {
   let batteryString = battery.toString() + "%";
   // Battery bar
-  batteryValue.width = Math.floor(battery) * device.screen.width / 100;
-  
+  _batteryBar.width = Math.floor(battery) * device.screen.width / 100;
+
   // Battery text
-  let max = batteries.length - 1;
-  for(let i=0; i<max; i++){
-    batteries[i+1].href = util.getImageFromLeft(batteryString,i);  
+  let max = _batteries.length - 1;
+  for (let i = 0; i < max; i++) {
+    _batteries[i + 1].href = util.getImageFromLeft(batteryString, i);
   }
 });
 // --------------------------------------------------------------------------------
@@ -90,52 +92,63 @@ batterySimple.initialize((battery)=>{
 // --------------------------------------------------------------------------------
 import * as simpleSettings from "./simple/device-settings";
 
-simpleSettings.initialize((data:any) => {
-  if (!data) {
+simpleSettings.initialize((settings: any) => {
+  if (!settings) {
     return;
   }
 
-  if (data.colorBackground) {
-    background.style.fill = data.colorBackground;
-    batteryBackground.gradient.colors.c2 = data.colorBackground;
+  if (settings.showBatteryPourcentage !== undefined) {
+    _batteriesContainer.style.display = settings.showBatteryPourcentage === true
+      ? "inline"
+      : "none";
+  }
+
+  if (settings.showBatteryBar !== undefined) {
+    _batteryBarContainer.style.display = settings.showBatteryBar === true
+      ? "inline"
+      : "none";
+  }
+
+  if (settings.colorBackground) {
+    background.style.fill = settings.colorBackground;
+    batteryBackground.gradient.colors.c2 = settings.colorBackground;
     UpdateActivities(); // For achivement color
   }
 
-  if (data.colorForeground) {
-    container.style.fill = data.colorForeground;
+  if (settings.colorForeground) {
+    container.style.fill = settings.colorForeground;
   }
 });
 // --------------------------------------------------------------------------------
 // Activity
 // --------------------------------------------------------------------------------
-import { goals,today } from "user-activity";
-let lastStepsGoals=-1;
-let lastSteps=-1;
+import { goals, today } from "user-activity";
+let lastStepsGoals = -1;
+let lastSteps = -1;
 
-goals.onreachgoal = (evt)=>{
+goals.onreachgoal = (evt) => {
   UpdateActivities();
 };
 
 // Update Activities informations
-function UpdateActivities()
-{
-  let actualStepsGoals = goals.steps||0;
-  let actualSteps = today.local.steps||0;
-  if(actualSteps != lastSteps
-    || actualStepsGoals != lastStepsGoals){
+function UpdateActivities() {
+  let actualStepsGoals = goals.steps || 0;
+  let actualSteps = today.local.steps || 0;
+  if (actualSteps != lastSteps
+    || actualStepsGoals != lastStepsGoals) {
     UpdateActivity(stepsContainer, actualStepsGoals, actualSteps);
     lastSteps = actualSteps;
     lastStepsGoals = actualStepsGoals;
   }
 }
 
-function UpdateActivity(container:GraphicsElement, goal:number, achieved:number) : void { 
+function UpdateActivity(container: GraphicsElement, goal: number, achieved: number): void {
   let achievedString = achieved.toString();
   let containers = container.getElementsByTagName("svg") as GraphicsElement[];
-  
+
   // Arc
   RenderActivity(containers[0], goal, achieved);
-  
+
   // Text
   // container.x = device.screen.width / 2 + 20 - (achievedString.toString().length * 20);
   let texts = containers[1].getElementsByTagName("image") as ImageElement[];
@@ -145,22 +158,22 @@ function UpdateActivity(container:GraphicsElement, goal:number, achieved:number)
 }
 
 // Render an activity
-function RenderActivity(container:GraphicsElement, goal:number, achieved:number):void {
+function RenderActivity(container: GraphicsElement, goal: number, achieved: number): void {
   let arc = container.getElementsByTagName("arc")[1] as ArcElement; // First Arc is used for background
   let circle = container.getElementsByTagName("circle")[0] as CircleElement;
   let image = container.getElementsByTagName("image")[0] as ImageElement;
 
   // Goals ok
-  if(achieved >= goal){
+  if (achieved >= goal) {
     circle.style.display = "inline";
-    arc.style.display= "none";
+    arc.style.display = "none";
     image.style.fill = background.style.fill;
   }
-  else{
+  else {
     circle.style.display = "none";
-    arc.style.display= "inline";
+    arc.style.display = "inline";
     arc.sweepAngle = util.activityToAngle(goal, achieved);
-    if(container.style.fill)
+    if (container.style.fill)
       image.style.fill = container.style.fill;
   }
 }
@@ -168,9 +181,9 @@ function RenderActivity(container:GraphicsElement, goal:number, achieved:number)
 // Heart rate manager
 // --------------------------------------------------------------------------------
 import * as simpleHRM from "./simple/hrm";
-let lastBpm:number;
+let lastBpm: number;
 
-simpleHRM.initialize((newValue, bpm, zone, restingHeartRate)=> {
+simpleHRM.initialize((newValue, bpm, zone, restingHeartRate) => {
   // Zones
   if (zone === "out-of-range") {
     imgHRM.href = "images/stat_hr_open_48px.png";
@@ -179,20 +192,20 @@ simpleHRM.initialize((newValue, bpm, zone, restingHeartRate)=> {
   }
 
   // Animation
-  if(newValue){
+  if (newValue) {
     iconHRM.animate("highlight");
   }
 
   // BPM value display
-  if(bpm !== lastBpm) {
+  if (bpm !== lastBpm) {
     if (bpm > 0) {
-      hrmContainer.style.display="inline";
+      hrmContainer.style.display = "inline";
       let bpmString = bpm.toString();
       hrmTexts[0].href = util.getImageFromLeft(bpmString, 0);
       hrmTexts[1].href = util.getImageFromLeft(bpmString, 1);
       hrmTexts[2].href = util.getImageFromLeft(bpmString, 2);
     } else {
-      hrmContainer.style.display="none";
+      hrmContainer.style.display = "none";
     }
   }
 });
